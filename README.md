8d9997862b648fee34af71fb71dd665252e1d6a3
Rachid Lamouri <rachidlamouri@gmail.com>
2024-07-31 22:46:10 -0400

    Add tsconfig and setup

---
+++ README.md
@@ -0,0 +1,15 @@
+# Notes
+
+## Setup
+
+```bash
+. ./setup.sh
+```
+
+## Example
+
+Save a picture/screenshot to `pics/` then run:
+
+```bash
+notes last
+```
--- setup.sh
+++ setup.sh
@@ -1,3 +1,15 @@
 #!bash
 
-alias notes="node index.js"
+nvm use
+
+mkdir -p pics
+echo "Pics directory exists"
+
+touch .metadata
+echo "Metadata file exists"
+
+mkdir -p backup
+echo "Backup directory exists"
+
+alias notes="npx ts-node src/index.js"
+echo "Created alias 'notes'"
